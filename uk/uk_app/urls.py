848b98f8ed32e5ba29b21519c524b13c8bf3f5fd
Uttam Kumar -X (uttkumar - CAPGEMINI AMERICA INC at Cisco) <uttkumar@partnet.cisco.com>
2019-06-18 16:52:50 +0530

    Modified urls.py

--- uk/uk_app/urls.py
+++ uk/uk_app/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns('',
     url(r'^storage-list?$', 'asgard_app.views.storge_list'),
     url(r'^pre_migrate?$', 'asgard_app.views.pre_migrate_list'),
-    url(r'migrate?$', 'asgard_app.views.migrate_list')
+    url(r'migrate?', 'asgard_app.views.migrate_list')
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
